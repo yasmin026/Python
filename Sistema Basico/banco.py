506ebd8f566c41128a57b8a0f33779d3eb16308c
Yasmin026 <ellenyasmin026@gmail.com>
2026-06-14 18:29:16 -0300

    criei um sistema de banco simples em Python. Ele inclui funcionalidades para criar contas, depositar, sacar e verificar o saldo. O código está organizado em uma classe chamada "Banco", que gerencia as operações bancárias. O sistema é projetado para ser fácil de usar e entender, permitindo que os usuários interajam com ele por meio de um menu simples.

--- Sistema Basico/banco.py
+++ Sistema Basico/banco.py
@@ -26,4 +26,30 @@ while True:
             print("❌ Entrada inválida. Por favor, digite um número válido.")
     elif opcao == '2':
         try:
-            valor
+            valor = float(input("Digite o valor do saque:R$"))
+            if valor > 0:
+                    if valor <= saldo:
+                        saldo -= valor
+                        extrato.append(f"- Saque: R$ {valor:.2f}")
+                        print("✅ Saque realizado com sucesso! Retire seu dinheiro.")
+                    else:
+                        print("❌ Saldo insuficiente para este saque.")
+            else:
+                    print("❌ O valor deve ser maior que zero.")
+        except ValueError:
+                print("❌ Valor inválido! Digite apenas números.")   
+    elif opcao == '3':
+        print("\n--- EXTRATO BANCÁRIO ---")
+        if len(extrato) == 0:
+            print("Nenhuma movimentação realizada hoje.")
+        else:
+            for movimento in extrato:
+                print(movimento)
+        print("------------------------")
+
+    elif opcao == '4':
+        print("Obrigado por usar o Banco Python. Até logo!")
+        break
+
+    else:
+        print("❌ Opção inválida! Digite 1, 2, 3 ou 4.") 
